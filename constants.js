c88cef6f727ea54795169ae5260018ee7febc5aa
Patrick Goethe <goetheproductions@hotmail.com>
2018-04-10 23:24:09 +0200

    adding endpoints

--- constants.js
+++ constants.js
@@ -1,1 +1,8 @@
 export const URI = 'mongodb://localhost:56651/shipster';
+export const ENDPOINT_PREFIX = '/api/v1';
+export const ENDPOINTS = {
+  users: `${ENDPOINT_PREFIX}/users`,
+  orders: `${ENDPOINT_PREFIX}/orders`,
+  user: `${ENDPOINT_PREFIX}/user=:token`,
+  fb_data: 'https://graph.facebook.com/v2.5/me?fields=email,first_name,last_name,picture,birthday&access_token=',
+};
